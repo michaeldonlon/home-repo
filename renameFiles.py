d07b666d81154ea48c8f34c40e7fd10d2d51021c
michaeldonlon <57276657+michaeldonlon@users.noreply.github.com>
2019-11-03 12:30:59 +1100

    Update and rename rename files.py to renameFiles.py

--- renameFiles.py
+++ renameFiles.py
@@ -9,11 +9,16 @@ root.wm_title("Choose file location")
 directory = tkinter.filedialog.askdirectory(title="Select location of files to rename")
 root.destroy()
 os.chdir(directory)
+# get a list of files in the cwd
 files = os.listdir(directory)
 
+# this is a pretty specific regex but it's a start for future scripts
 fileNameRegex = re.compile(r'(SAR-ED-)(20)(-)(\d\d)(\d\d)(.*)')
+
+# initialise any iterating strings to go in the filename here
 i = 33
 
+# iterate through the files
 for item in files:
     fileName = str(item)
     fileNameMo = fileNameRegex.search(fileName)
@@ -25,15 +30,10 @@ for item in files:
 #    print(fileNameMo.group(5))
 #    print(fileNameMo.group(6))
 
+#    try change the first line below to this:
+#    os.rename(directory + "/" + fileName,
     os.rename(directory + "/" + fileNameMo.group(0),
               directory + "/" + fileNameMo.group(1) + "29" + fileNameMo.group(3) +
               "04" + str(i) + fileNameMo.group(6))
 
-
-#    print(directory + "/" + fileNameMo.group(0))
-#    print(directory + "/" + fileNameMo.group(1) + "29" + fileNameMo.group(3) +
-#              "04" + str(i) + fileNameMo.group(6))
-
-#    print(i)
     i = i + 1
-
